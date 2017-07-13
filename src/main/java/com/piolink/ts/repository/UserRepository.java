91a5e27a51f61101fbf2d131a83bf287c61c6d1e
lagoonKmj <kmj30801@gmail.com>
2017-07-13 10:30:45 +0900

    User 관계 제거로 인한 클래스 삭제

--- src/main/java/com/piolink/ts/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.piolink.ts.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.piolink.ts.domain.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
