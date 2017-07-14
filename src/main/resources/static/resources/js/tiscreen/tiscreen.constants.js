eb586b35bba5f042af8d3f3b1ff6d04947ad3fb7
lagoonKmj <kmj30801@gmail.com>
2017-07-14 15:56:49 +0900

    상수 오브젝 클래스 추가

---
+++ src/main/resources/static/resources/js/tiscreen/tiscreen.constants.js
@@ -0,0 +1,8 @@
+var tiConstant = Object.freeze({
+      "ORIGINAL_DASHBOARD_ID" : 0
+    , "ORIGINAL_DASHBOARD_NAME" : "통합 대시보드"
+    , "BASIC_DASHBOARD_ID_STARTWITH" : 10000
+    , "BASIC_DASHBOARD_USER_ID" : -1
+    , "BASIC_DASHBOARD_ADD" : 0
+    , "NEW_DASHBOARD_ADD" : 1
+});
